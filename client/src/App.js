c5f7a9e3b29e0135c8a214e146baf2fc5ee5d9cb
Emily Yu <bluerainmango@gmail.com>
2020-04-03 21:00:15 -0700

    test Carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component, Fragment } from 'react';
-import './App.css';
-import Navbar from './navbar/navbar.js';
-import Carousel from './carousel/carousel.js';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Navbar from "./navbar/navbar.js";
+// import Carousel from './carousel/carousel.js';
+import { MovieCarousel } from "./components/carousel/carousel.component";
 
 function App() {
   return (
-    <div className="App App-body" >
+    <div className="App App-body">
       <Navbar />
-      <Carousel />
+      <MovieCarousel />
     </div>
   );
 }
