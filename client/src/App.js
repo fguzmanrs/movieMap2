8b15e86e6b5b12914f97e2fc9edc1ec5044c6732
Tiresias <fguzmanrs@gmail.com>
2020-04-03 18:46:26 -0700

    added component skeleton
    signUp
    signIn
    in-line grid carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Navbar from './navbar/navbar.js';
+import Carousel from './carousel/carousel.js';
 
 function App() {
   return (
     <div className="App App-body" >
       <Navbar />
+      <Carousel />
     </div>
   );
 }
