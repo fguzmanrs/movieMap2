37ecba17396d466d50044d4e5a1f054b78374adc
HCross <haydenlcrossjunk@gmail.com>
2020-04-11 10:30:04 -0700

    adding styles to card

--- client/src/filmCard/filmCard.js
+++ client/src/filmCard/filmCard.js
@@ -12,7 +12,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
+/*import ShareIcon from '@material-ui/icons/Share';*/
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
@@ -46,60 +46,73 @@ export default function FilmReviewCard() {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-
+//rgba(210, 204, 243, 0.816)
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="film" className={classes.avatar}>
-            PG-13
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Movie Title"
-      />
-      <CardMedia
-        className={classes.media}
-        image=""
-        title="Movie Poster"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Film Details</Typography>
+    <Card className={classes.root} style={{backgroundColor: '#99a0f9', border: "none", boxShadow: '0px -5px 20px 20px #99a0f9', borderRadius: 'none'}}>
+        <CardHeader style={{backgroundColor: '#99a0f9', color: "white"}}
+          avatar={
+            <Avatar aria-label="film" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+
+          action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+          }
+
+          action={
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+          }
+          
+          title={<div class="headerTitle" style={{fontSize:'34px', fontWeight: 'bold'}}>Movie Title</div>
+            
+          }
+        />
+      
+
+        <CardContent style={{
+          padding: '0px',
+          backgroundColor: 'rgba(210, 204, 243, 0.816)'
+          }}>
+      
+
+          <div class="right-block" style={{
+            float: 'right',
+            width: '70%',
+            height: '500px'
+            
+          }} >
+            
+            <img src={"https://www.moviepostersusa.com/media/easyslide/rise-of-skywalker.jpg"} alt="poster" style={{height: '500px', width:'100%'}} />
+
+          </div>
+
+          <div class="left-block" style={{ 
+            color: '#fff', 
+            //backgroundColor: "rgba(210, 204, 243, 0.816)",
+            backgroundColor: "#99a0f9",
+            float: 'right',
+            width: '30%',
+            height: '500px',
+            boxShadow: '100px 0px 150px 250px #99a0f9'
+            }} >
           <Typography>Summary</Typography>
           <Typography>Rating</Typography>
           <Typography>Genre</Typography>
           <Typography>Cast</Typography>
           <Typography>Trailer</Typography>
           <Typography>Watch Options</Typography>
+          </div>
+        
+
         </CardContent>
-      </Collapse>
     </Card>
   );
 }
+
+//https://www.moviepostersusa.com/media/easyslide/rise-of-skywalker.jpg
+//https://cdn.cinematerial.com/p/297x/haeorwgk/1917-british-movie-poster-md.jpg?v=1579166770
