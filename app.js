baad37eccbd03c919b1e133f7992b8efc355719a
Emily Yu <bluerainmango@gmail.com>
2020-04-16 16:36:35 -0700

    changed static path

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use(cors(corsOptionsDelegate));
 app.use(express.static("client/build"));
 
 // server server's user profile images for frontend
-app.use(express.static(path.join(__dirname, "/public/assets/images")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Body parser
 app.use(express.json());
