e0d5276aa4399acbe0822edfe17893aa275305b4
Emily Yu <bluerainmango@gmail.com>
2020-04-16 16:24:20 -0700

    added build folder

---
+++ client/build/precache-manifest.014fa7ecd778af77853c20ff1c8de8a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22048d71157edcad80c06b95e49d825c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91f13e79b3195a32f87",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "c51050c02e9d30349fd1",
+    "url": "/static/css/main.8ff61be0.chunk.css"
+  },
+  {
+    "revision": "b91f13e79b3195a32f87",
+    "url": "/static/js/2.e15572bb.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.e15572bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51050c02e9d30349fd1",
+    "url": "/static/js/main.6430214a.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
