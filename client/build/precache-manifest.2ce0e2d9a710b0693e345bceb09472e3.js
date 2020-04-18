7c78827db2fb5377f00c26c619ee5cb5fba950d7
Fernando Ortiz <ffortizn@gmail.com>
2020-04-17 23:40:58 -0700

    added images to README.md

---
+++ client/build/precache-manifest.2ce0e2d9a710b0693e345bceb09472e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9050e97fa050bbd4868d6a5cd87583b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5db418afce5dc60b982",
+    "url": "/static/css/2.37c00e0b.chunk.css"
+  },
+  {
+    "revision": "69dd308a6a53956f5f35",
+    "url": "/static/css/main.0e4b0d21.chunk.css"
+  },
+  {
+    "revision": "d5db418afce5dc60b982",
+    "url": "/static/js/2.9de50ee4.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.9de50ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69dd308a6a53956f5f35",
+    "url": "/static/js/main.ab704278.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
