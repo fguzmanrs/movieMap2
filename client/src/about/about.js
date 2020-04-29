0bee03abe66cc11c8d52a5d1a2793d409f7880b6
Tiresias <fguzmanrs@gmail.com>
2020-04-28 19:15:15 -0700

    about updates

--- client/src/about/about.js
+++ client/src/about/about.js
@@ -34,39 +34,39 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const cards = [
-{
-    img:"",
+  {
+    img: "",
     member: "Emily Yu",
     role: "Full-Stack Developer",
-    bio: "In wordDoc", 
-    github: "https://github.com/bluerainmango", 
+    bio: "After 5 years of working in E-commerce and Online Marketing, Emily has brought her experience into a new career as a Full-Stack Developer. A recent graduate of UC, Riverside Extension: Coding, she is already applying her new skills to front-end and back-end development projects. Her dedication to exploring new ideas and skills is visible in her love of international cuisine, travel, and the various languages she knows (linguistic and coding). Her diligent and passionate approach makes her a valuable team member that brings together the team's vision, designs, and technology together.",
+    github: "https://github.com/bluerainmango",
     linkedin: "https://www.linkedin.com/in/bluerainmango/",
     portfolio: "https://emily-yu-portfolio.herokuapp.com/"
   },
   {
-    img:"In wordDoc",
+    img: "In wordDoc",
     member: "Fabiola Guzman",
     role: "Front-End Developer",
-    bio: "", 
-    github: "GITHUB", 
+    bio: "",
+    github: "GITHUB",
     linkedin: "LINKEDIN",
     portfolio: "PORTFOLIO"
   },
   {
-    img:"",
+    img: "",
     member: "Francisco Ortiz",
     role: "Full-Stack Developer",
-    bio: "In wordDoc", 
-    github: "https://github.com/ffortizn", 
+    bio: "In wordDoc",
+    github: "https://github.com/ffortizn",
     linkedin: "https://www.linkedin.com/in/ffortizn/",
     portfolio: "https://ffortizn.github.io/portfolio.html"
   },
   {
-    img:"",
+    img: "",
     member: "Hayden Cross",
     role: "Front-End Developer",
-    bio: "In wordDoc", 
-    github: "https://github.com/HCross28", 
+    bio: "In wordDoc",
+    github: "https://github.com/HCross28",
     linkedin: "https://www.linkedin.com/in/hayden-cross-86803a19a/",
     portfolio: "https://hcross28.github.io/2nd-Profile-Update/index.html"
   }
@@ -80,7 +80,7 @@ export default function Album() {
       <CssBaseline />
       <main>
         {/* Hero unit */}
-        <div className={classes.heroContent} style = {{backgroundColor: "#BCE0EF", borderBottom: "5px solid #8BAEBD"}}>
+        <div className={classes.heroContent} style={{ backgroundColor: "#BCE0EF", borderBottom: "5px solid #8BAEBD" }}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
               Our Story
@@ -114,13 +114,13 @@ export default function Album() {
                   </CardContent>
                   <CardActions>
                     <Button size="small" color="primary">
-                      < Link>{card.github}</Link>
+                      < Link href={'{card.github}'} target="_blank">Github</Link>
                     </Button>
                     <Button size="small" color="primary">
-                      < Link>{card.linkedin}</Link>
+                      < Link href={'{card.linkedin}'} target="_blank">LinkedIn</Link>
                     </Button>
                     <Button size="small" color="primary">
-                      < Link>{card.portfolio}</Link>
+                      < Link href={'{card.portfolio}'} target="_blank">Portfolio</Link>
                     </Button>
                   </CardActions>
                 </Card>
