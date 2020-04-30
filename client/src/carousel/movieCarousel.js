c32e393cc65bf0860c88af104b6f952a1b4614c0
Emily Yu <bluerainmango@gmail.com>
2020-04-29 18:04:03 -0700

    fixed a bug: some of TMDB data's poster path has '/' so validate it not to have broken image

--- client/src/carousel/movieCarousel.js
+++ client/src/carousel/movieCarousel.js
@@ -143,7 +143,11 @@ const MovieCarousel = (props) => {
               >
                 {console.log("when render b", showCard, cardIndex)}
                 <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  src={`https://image.tmdb.org/t/p/w500/${
+                    movie.poster_path.substr(0, 1) === "/"
+                      ? movie.poster_path.slice(1)
+                      : movie.poster_path
+                  }`}
                   className="poster"
                   style={{ cursor: "pointer" }}
                   onClick={handleClick}
